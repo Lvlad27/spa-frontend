9b94397e8a0face929a7f5c0840689d8513266eb
vladleontescu <vladleontescu@coherentsolutions.com>
2022-04-07 20:39:18 +0300

    fix input error messages

--- js/formValidation.js
+++ js/formValidation.js
@@ -158,44 +158,73 @@ class FormValidation {
     validateFormInputs(formInput) {
         // Check if any values exist
         if (formInput.value.trim() === '') {
-            // console.log(formInput);
-            console.log(formInput.previousElementSibling);
-
-            this.setStatus(formInput, `${formInput.placeholder} can't be blank`, 'error');
+            this.setStatus(formInput, `Please fill in ${formInput.placeholder}`, 'error');
+            formInput.style.borderBottom = '2px #ff6188 solid';
         } else {
             this.setStatus(formInput, null, 'success');
         }
 
         // Check for a valid email address
         if (formInput.type === 'text') {
-            const mailRegEx = /\S+@\S+\.\S+/;
+            const mailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
             if (mailRegEx.test(formInput.value)) {
                 this.setStatus(formInput, null, 'success');
+                formInput.style.borderBottom = '2px #ff6188 solid';
             } else {
                 this.setStatus(formInput, 'Please enter valid email address', 'error');
+                formInput.style.borderBottom = '2px #ff6188 solid';
             }
         }
 
         // Check for valid password
         if (formInput.type === 'password') {
-            const min = 3;
+            const min = 3,
+                stringOne = 'Should contain at least a digit and a letter',
+                stringTwo = `Should contain at least ${min} characters`,
+                regexOne = /^(?=.*[a-zA-Z]).{0,3}$/,
+                regexTwo = /^(?=.*[a-zA-Z]).{3,}$/,
+                regexThree = /(?=.*[0-9]).{0,3}$/,
+                regexFour = /(?=.*[0-9]).{3,}$/,
+                regexFive = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+).{0,3}$/,
+                regexSix = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+).{2,}$/;
 
-            if (!passwordRegEx.test(formInputs.value)) {
-                this.setStatus(formInput, `Must contain at least one number and letter`, 'error');
-            } else if (formInput.value < min) {
-                this.setStatus(formInput, `Must contain at least ${min} characters`, 'error');
-            } else {
+            // case1 - only letters and less than 3
+            // case3 - only numbers and less than 3
+            if (regexOne.test(formInput.value) || regexThree.test(formInput.value)) {
+                this.setStatus(formInput, `${stringOne}\n${stringTwo}`, 'error');
+                formInput.style.borderBottom = '2px #ff6188 solid';
+            }
+
+            // case2 - only letters and more than 3
+            // case4 - only numbers and more than 3
+            if (regexTwo.test(formInput.value) || regexFour.test(formInput.value)) {
+                this.setStatus(formInput, `${stringOne}`, 'error');
+                formInput.style.borderBottom = '2px #ff6188 solid';
+            }
+
+            // case5 - smaller than 3 but with numbers and letters
+            if (regexFive.test(formInput.value)) {
+                this.setStatus(formInput, `${stringTwo}`, 'error');
+                formInput.style.borderBottom = '2px #ff6188 solid';
+            }
+
+            // case6 - success
+            if (regexSix.test(formInput.value)) {
                 this.setStatus(formInput, null, 'success');
+                formInput.style.borderBottom = '2px #a9dc76 solid';
             }
         }
 
         // Check for password confirmation
         if (formInput.id === 'passwordConfirm') {
+            const passwordInput = this.form.querySelector('#password');
             if (formInput.value.trim() == '') {
                 this.setStatus(formInput, 'Confirmation required', 'error');
-            } else if (formInput.value != password.value) {
+                formInput.style.borderBottom = '2px #ff6188 solid';
+            } else if (formInput.value !== passwordConfirm.value) {
                 this.setStatus(formInput, "Doesn't match", 'error');
+                formInput.style.borderBottom = '2px #ff6188 solid';
             } else {
                 this.setStatus(formInput, null, 'success');
             }
