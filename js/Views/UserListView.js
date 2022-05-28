6239aec1777e9c2f5cda1e21f0807a138d17c94e
vladleontescu <vladleontescu@coherentsolutions.com>
2022-05-28 17:04:34 +0300

    feat: add image grid to user form

--- js/Views/UserListView.js
+++ js/Views/UserListView.js
@@ -4,8 +4,6 @@ class UserListView extends BaseView {
     constructor(DataService, templateRenderer) {
         super(DataService, templateRenderer);
         this.template = document.getElementById('UserListView');
-
-        document.addEventListener('click', this.$deleteBtn.bind(this));
     }
 
     async getData() {
@@ -21,17 +19,6 @@ class UserListView extends BaseView {
             return dataObj;
         } catch (error) {
             console.error(error);
-        }
-    }
-
-    async $deleteBtn(event) {
-        if (event.target.matches('#deleteUserBtn')) {
-            const userToBeDeleted =
-                event.target.parentElement.parentElement.firstElementChild.firstElementChild
-                    .innerHTML;
-
-            // if(userToBeDeleted === )
-            await this.DataService.deleteUser(userToBeDeleted);
         }
     }
 }
